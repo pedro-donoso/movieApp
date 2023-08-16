2fdc71b998b58d1e12e12a45aa0d4686eeeb67ea
pedro-donoso <pedro.donoso.r@usach.cl>
2023-08-16 17:34:08 -0400

    correccion seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,21 +8,21 @@
 
 20.times do
     Movie.create(name: Faker::Movie.title,
-                synopsis: Fakker::Movie.quote,
-                director: Fakker::Name.name,
+                synopsis: Faker::Movie.quote,
+                director: Faker::Name.name,
     )
     end
     
     20.times do
     Serie.create(name: Faker::Serie.title,
-                synopsis: Fakker::Serie.quote,
-                director: Fakker::Name.name,
+                synopsis: Faker::Serie.quote,
+                director: Faker::Name.name,
     )
     end
     
     20.times do
     Documentary_film.create(name: Faker::Documentary_film.title,
-                synopsis: Fakker::Movie.quote,
-                director: Fakker::Name.name,
+                synopsis: Faker::Movie.quote,
+                director: Faker::Name.name,
     )
     end
