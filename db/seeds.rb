df80919c897a33c8815b7a7d4aa4300322dac774
pedro-donoso <pedro.donoso.r@usach.cl>
2023-08-16 17:28:39 -0400

    agrego seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+20.times do
+    Movie.create(name: Faker::Movie.title,
+                synopsis: Fakker::Movie.quote,
+                director: Fakker::Name.name,
+    )
+    end
+    
+    20.times do
+    Serie.create(name: Faker::Serie.title,
+                synopsis: Fakker::Serie.quote,
+                director: Fakker::Name.name,
+    )
+    end
+    
+    20.times do
+    Documentary_film.create(name: Faker::Documentary_film.title,
+                synopsis: Fakker::Movie.quote,
+                director: Fakker::Name.name,
+    )
+    end
